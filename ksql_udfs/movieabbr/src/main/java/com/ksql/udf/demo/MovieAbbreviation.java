6e9858093e1e6978855a60e04a9a0e8130612c13
Gunjan Kaphle <gunjan.kaphle@therealreal.com>
2019-12-18 21:20:14 -0800

    Update pom

---
+++ ksql_udfs/movieabbr/src/main/java/com/ksql/udf/demo/MovieAbbreviation.java
@@ -0,0 +1,26 @@
+package com.ksql.udf.demo;
+
+import io.confluent.ksql.function.udf.Udf;
+import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.function.udf.UdfParameter;
+
+@UdfDescription(name = "movieAbbr", description = "creates movie abrreviation from name")
+public class MovieAbbreviation {
+	
+	  @Udf(description = "Takes a string and creates string abbreviation")
+	  public String movieAbbr(
+	    @UdfParameter(value = "movieAbbr", description = "movie name") final String movieName) {
+	    
+		// split the movie name
+	    String[] allWords = movieName.split(" ");
+	    
+	    // build a new string with first characters
+	    StringBuilder finalAbbr = new StringBuilder();
+	    for (String word: allWords) {
+	    	finalAbbr.append(word.charAt(0));
+	    }
+	    
+	    // return the new string in Upper case
+		return finalAbbr.toString().toUpperCase();
+	  }
+}
